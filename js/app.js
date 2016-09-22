7949e2f2a3a38f1a37af967f12489ee37b334ea8
biffyn <nuravenweb@gmail.com>
2016-09-23 01:30:34 +0200

    Added quote on load and on click function

--- js/app.js
+++ js/app.js
@@ -1,19 +1,29 @@
 $(document).ready(function() {
-    // vars
-    // var author = "",
-    //     quote = "";
-
     // retrieve a quote
-    function retrieveQuote() {
-        return $.ajax({
-            // request
-            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    function requestQuote() {
+        // request quote
+        $.ajax({
+            url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
             headers: {
-                'X-Mashape-Key': '9NrMW3A7TOmsheYkGNEnHoVHm8IOp1NzIfrjsnbAowyQUTXAvW',
-                method: 'POST',
-                contentType: 'application/x-www-form-urlencoded',
-                dataType: 'json'
+                "X-Mashape-Key": "9NrMW3A7TOmsheYkGNEnHoVHm8IOp1NzIfrjsnbAowyQUTXAvW",
+                method: "POST",
+                contentType: "application/x-www-form-urlencoded",
+                dataType: "json"
             },
+            // parse and save requested response to variables and pass to frontend
+            success: function(response) {
+                var res = JSON.parse(response);
+                quote = res.quote;
+                author = res.author;
+                $(".quote-text").html(quote);
+                $(".author").html("– " + author);
+            }
         });
     }
+    // request quote on page load
+    requestQuote();
+    // request new quote when New Quote button is click
+    $("#quote-btn").on("click", function(){
+        requestQuote();
+    });
 });
