0a4c51df0d7a98ad1e7d0cf937bb6d59905d55fe
biffyn <nuravenweb@gmail.com>
2016-09-22 23:09:38 +0200

    style changes - add get quote function

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-// new quote on click
+$(document).ready(function() {
+    // vars
+    // var author = "",
+    //     quote = "";
+
+    // retrieve a quote
+    function retrieveQuote() {
+        return $.ajax({
+            // request
+            url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+            headers: {
+                'X-Mashape-Key': '9NrMW3A7TOmsheYkGNEnHoVHm8IOp1NzIfrjsnbAowyQUTXAvW',
+                method: 'POST',
+                contentType: 'application/x-www-form-urlencoded',
+                dataType: 'json'
+            },
+        });
+    }
+});
