f1e67bc6fae1e5bf84920b3567a049f64ac44baf
biffyn <nuravenweb@gmail.com>
2016-09-23 03:36:06 +0200

    add share to twitter function

--- js/app.js
+++ js/app.js
@@ -1,7 +1,14 @@
+/******************************
+  Quote.R - a random quote machine
+  - James Richards
+******************************/
 $(document).ready(function() {
-    // retrieve a quote
     function requestQuote() {
-        // request quote
+        /*
+        * requests a new quote object from andruxnet-random-famous-quotes API via ajax
+        * On success parses and saves request to variables and
+        * renders to the frontend
+        */
         $.ajax({
             url: "https://andruxnet-random-famous-quotes.p.mashape.com/cat=",
             headers: {
@@ -15,15 +22,32 @@ $(document).ready(function() {
                 var res = JSON.parse(response);
                 quote = res.quote;
                 author = res.author;
-                $(".quote-text").html(quote);
-                $(".author").html("– " + author);
+                // when passed the quote, faded it in
+                var a = $.Deferred();
+                var b = $.Deferred();
+                $(".quote-text").fadeOut(500, function() {          // display quote
+                    $(this).text(quote).fadeIn(500, function() {
+                        a.resolve();
+                    });
+                });
+                $(".author").fadeOut(500, function() {              // display quote author
+                    $(this).text("– " + author).fadeIn(500, function() {
+                        b.resolve();
+                    });
+                });
             }
         });
     }
     // request quote on page load
     requestQuote();
     // request new quote when New Quote button is click
-    $("#quote-btn").on("click", function(){
+    $("#quote-btn").on("click", function(event) {
+        event.preventDefault();
         requestQuote();
+    });
+    // share to twitter when Tweet This Quote button is pressed
+    $("#tweet-btn").on("click", function(event){
+        event.preventDefault();
+        window.open("https://twitter.com/intent/tweet?text=" + encodeURIComponent('"' + quote + '" ' + "– " + author));
     });
 });
